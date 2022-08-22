ba3dd107965e19e24e117f63231358aa36f2de0a
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-21 22:56:18 -0300

    position class

---
+++ src/board/Position.java
@@ -0,0 +1,38 @@
+package board;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    // define posição
+    public void setPosition(int row, int column) {
+        setRow(row);
+        setColumn(column);
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
