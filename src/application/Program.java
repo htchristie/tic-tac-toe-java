ac701ba4c84a687f2e0b1ee0e4d1a07413f1f085
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-21 22:56:06 -0300

    initial files

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+    }
+}
