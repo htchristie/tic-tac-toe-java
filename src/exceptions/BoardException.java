a8c123ec42d1be20fc2c199ae483d758fb53be9b
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-21 23:55:46 -0300

    add comments

--- src/exceptions/BoardException.java
+++ src/exceptions/BoardException.java
@@ -2,6 +2,7 @@ package exceptions;
 
 public class BoardException extends RuntimeException {
 
+    // repassa a mensagem de erro pro construtor do suoer
     public BoardException(String msg) {
         super(msg);
     }
