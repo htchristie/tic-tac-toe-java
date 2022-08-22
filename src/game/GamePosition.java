f3171fdedeb424acf7d70020f7e189258fa3c7bf
Cristyane Tamioso <christiehespanha@gmail.com>
2022-08-22 10:27:40 -0300

    GamePosition class

---
+++ src/game/GamePosition.java
@@ -0,0 +1,44 @@
+package game;
+
+import board.Position;
+import exceptions.GameException;
+
+public class GamePosition {
+
+    // utiliza posição do tabuleiro do jogo ex. a3
+    private char column;
+    private int row;
+
+    public GamePosition(char column, int row) {
+        if (column < 'a' || column > 'c' || row < 1 || row > 3) {
+            throw new GameException("Invalid position: valid values range from a1 to c3.");
+        }
+
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    // transforma posição do tabuleiro em posição de matriz
+    protected Position toPosition() {
+        return new Position(3 - row, column - 'a');
+    }
+
+    // transforma posição de matriz em posição do tabuleiro
+    protected static GamePosition fromPosition(Position position) {
+        return new GamePosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+        // "" serve pra avisar que o texto deve ser impresso como String (row é int)
+    }
+}
